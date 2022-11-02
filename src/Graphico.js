a89d45bbca17cea24fc9f0c12922885131371703
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-11-01 18:08:10 -0700

    vcard reupdate

--- src/Graphico.js
+++ src/Graphico.js
@@ -7,7 +7,7 @@ export const Graphico = () => {
     <div className='graphico'>
        <p> <img src='/Concepto-AZ-Logo.png'style={{"height" : "100px", "width" : "250px"}} onClick={()=>window.open('www.conceptoaz.com', '_blank')}></img></p>
          <p style={{"marginTop" : "50px", "marginBottom" : "50px"}} ><em>THANKS FOR SCANNING ~ !</em></p>
-    <a href='/contact.vcf' download><button id='contact-btn'><b>Add to Contacts</b></button></a>
+    <a href='/graphico.vcf' download><button id='contact-btn'><b>Add to Contacts</b></button></a>
 
 
 
