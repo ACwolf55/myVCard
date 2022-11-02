ce6d76bab5bdfc828aefb9b7fad9aa0c969b824c
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-11-02 14:28:18 -0700

    plumber vcf

--- src/Plumber.js
+++ src/Plumber.js
@@ -6,7 +6,7 @@ export const Plumber = () => {
 
     <p><img src='/plumberLogo.jpg'style={{"height" : "100px", "width" : "250px"}} onClick={()=>window.open('https://1800plumber.com/locations/scottsdale-az', '_blank')}></img></p>   
      <p style={{"marginTop" : "50px", "marginBottom" : "50px"}} ><em>THANKS FOR SCANNING ~ !</em></p>
-     <a href='/contact.vcf' download><button id='contact-btn'><b>Add to Contacts</b></button></a>
+     <a href='/1800plumber.vcf' download><button id='contact-btn'><b>Add to Contacts</b></button></a>
  
  
  
