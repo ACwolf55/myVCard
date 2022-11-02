ed0063092f1bddcac6710b4618d1c3871a726be0
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-11-02 14:25:27 -0700

    plumber page add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Routes,Route} from 'react-router-dom'
 import { Home } from './Home';
 import { Graphico } from './Graphico';
+import { Plumber } from './Plumber';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 <Routes>
     <Route path='/' element={ <Home />} />
     <Route path='/graphico' element={ <Graphico/> } />
+    <Route path='/1800plumber' element={ <Plumber/> } />
   </Routes>
     </div>
   );
---
+++ src/Plumber.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Plumber = () => {
+  return (
+    <div>
+
+    <p><img src='/plumberLogo.jpg'style={{"height" : "100px", "width" : "250px"}} onClick={()=>window.open('https://1800plumber.com/locations/scottsdale-az', '_blank')}></img></p>   
+     <p style={{"marginTop" : "50px", "marginBottom" : "50px"}} ><em>THANKS FOR SCANNING ~ !</em></p>
+     <a href='/contact.vcf' download><button id='contact-btn'><b>Add to Contacts</b></button></a>
+ 
+ 
+ 
+     </div>
+  )
+}
