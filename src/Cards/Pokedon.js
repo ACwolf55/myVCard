71fdd0989781736fa70c344e8148665bcc0f8ce0
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-11-04 14:18:06 -0700

    pokedon logo fix

--- src/Cards/Pokedon.js
+++ src/Cards/Pokedon.js
@@ -6,7 +6,7 @@ export const Pokedon = () => {
 
   return (
     <div >
-       <p> <img src='/pokedonlogo.png'style={{"height" : "100px", "width" : "250px"}} onClick={()=>window.open('https://pokedonaz.com/#', '_blank')}></img></p>
+       <p> <img src='/logo-pokedon.png'style={{"height" : "100px", "width" : "250px"}} onClick={()=>window.open('https://pokedonaz.com/#', '_blank')}></img></p>
          <p style={{"marginTop" : "50px", "marginBottom" : "50px"}} ><em>THANKS FOR SCANNING ~ !</em></p>
     <a href='/Pokedon.vcf' download><button id='contact-btn'><b>Add to Contacts</b></button></a>
 
